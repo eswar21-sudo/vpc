d5514c90817d5d276be59cacc5c4fccb0ba3566b
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-12 09:13:07 +0000

    files updated

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a" # Change to a valid AZ
+  availability_zone       = "us-east-1b" # Change to a valid AZ
 
   tags = {
     Name = "PublicSubnet"
